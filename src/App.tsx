7ffdb2a9bbfe50f1ce37abbdf85615a76dc33ec2
ashikfifa <ashikahmedfifa@gmail.com>
2023-01-18 19:11:24 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProjectsPage from "./projects/ProjectsPage";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="container">
+      <ProjectsPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/projects/ProjectsPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { MOCK_PROJECTS } from "./MockProjects";
+import ProjectList from "./ProjectList";
+
+function ProjectsPage() {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <ProjectList projects={MOCK_PROJECTS} />
+    </div>
+  );
+}
+
+export default ProjectsPage;
